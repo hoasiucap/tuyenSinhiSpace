da7650122be08dd22cce27b593822a1beb187995
bth060403 <95699610+bth060403@users.noreply.github.com>
2023-09-25 07:09:33 +0700

    Updates

--- src/admin/access/js/page.js
+++
@@ -1,6 +0,0 @@
-// var el = document.getElementById("wrapper");
-// var toggleButton1 = document.getElementById("menu-toggle");
-
-// toggleButton1.onclick = function() {
-//     el.classList.toggle("toggled");
-// };
